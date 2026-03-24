f836cafaeada959ca806ea4c8c9f93c71de52489
RymonPuffs <rymon344@gmail.com>
2026-03-24 11:10:26 -0700

    a

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("TILINNN")
+from enums import TipoProducto
+print("bbienvenido ala maquina de Jisus crais soy jodido")
+print("-------------------------------------------------")
+print("ELija el producto que desea comprar")
+decicion1 = int(input())
+if decicion1 == 1:
+    
