d8e60b8e5e5426ddd0b24b1959c08ea7d79ba3e1
ImVid-glitch <gersonpp75@gmail.com>
2026-03-24 11:19:04 -0700

    ola

---
+++ entidades/Compra.py
@@ -0,0 +1,9 @@
+from entidades.Producto import Producto
+
+class Compra():
+    def __init__(self, presupuesto: float, cambio: float):
+        presupuesto = presupuesto 
+        cambio = cambio
+
+    def proceso_compra(Compra):
+        super
---
+++ entidades/Producto.py
@@ -0,0 +1,11 @@
+from enums import TipoProducto
+class Producto():
+
+    def __init__(self, nombre: str, precio: float, cantidad:int, tipo: TipoProducto):
+        nombre = nombre
+        precio = precio
+        cantidad = cantidad
+        tipo = tipo
+        
+    
+        
